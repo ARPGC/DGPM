ce23a69c3e36be7335204cd92a8f45eff4768422
ARPGC <ecobirlaapp@gmail.com>
2026-01-13 14:24:50 +0530

    Create config2.js

---
+++ config2.js
@@ -0,0 +1,12 @@
+const CONFIG_REALTIME = {
+    // Your New Realtime Project URL
+    url: "https://mpvcqvghbnizjkjdjixz.supabase.co",
+
+    // Public Key (For Students to READ) - You provided this
+    anonKey: "sb_publishable_kPUuDxQv2S8PeusfeFFEEw_QLDZpJAM",
+
+    // Service Role Key (For Admin to WRITE) - REQUIRED for the "Relay" to work
+    // Go to Supabase Dashboard -> Project Settings -> API -> service_role (secret)
+    // Copy that key and paste it here:
+    serviceKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1wdmNxdmdoYm5pempramRqaXh6Iiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc2ODI5MzkzMSwiZXhwIjoyMDgzODY5OTMxfQ.HkqeEEYBDcjHjUmJoxo1iF2sD-HhJgh6CtMGIqXmWAs" 
+};
